1252e1f3077a6d6a46b16039bbf83bd7e94bdb31
KateEHowlett <kate.howlett@norfolk.gov.uk>
2023-08-11 12:14:10 +0100

    Users section of tasks all working

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const app = express();
-const {User} = require("../models/user")
+const {User, Show} = require("../models/index")
 const {db} = require("../db/connection");
 
 app.get("/users", async (request,response) => {
     let users = await User.findAll({});
     response.json(users);
+})
+
+app.get("/users/:id", async (request,response) => {
+    const id = request.params.id;
+    const user = await User.findByPk(id);
+    response.json(user)
+})
+
+app.get("/users/:id/watched", async (request,response) => {
+    const userID = request.params.id;
+    const user = await User.findByPk(userID);
+    const shows = await user.getShows({joinTableAttributes:[]});
+    response.json(shows);
+})
+
+app.put("/users/:id/watched/:showid", async (request,response) => {
+    const user = await User.findByPk(request.params.id);
+    await user.addShow(request.params.showid);
+    const shows = await user.getShows({joinTableAttributes:[]});
+    response.json(shows)
 })
 
 
