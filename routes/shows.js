67828b11050019ecf8016cc9b44f28bc2e428782
nimonian <nimon@hotmail.co.uk>
2023-08-11 17:03:58 +0100

    fixes the checker middleware

--- routes/shows.js
+++ routes/shows.js
@@ -1,71 +1,82 @@
-const express = require("express");
-const router = express.Router();
-const { Show } = require("../models/index")
-const {db} = require("../db/connection");
+const express = require('express')
+const router = express.Router()
+const { Show } = require('../models/index')
+const { db } = require('../db/connection')
 
-// Homebrew middleware which we HOPED would validate the parameters and throw errors if they weren't correct
-const checker = async function(request,response,next){
-    const parameter = await String(request.params.status)
-    if(parameter===""||parameter===null||parameter===undefined){
-        console.log("oops: " + parameter)
-        throw new Error;
-    }
-    console.log("yays" + parameter)
-    next()
+const checker = function (request, response, next) {
+  const { status } = request.params
+
+  // make sure status exists and fail fast
+  if (!status) {
+    response.status(400).send({ error: 'No status found' })
+    return
+  }
+
+  // valid statuses
+  const statuses = ['running', 'cancelled']
+
+  // if status not a valid choice, yell at the dev!
+  if (!statuses.includes(status)) {
+    response.status(400).send({ error: 'Must be a valid status', statuses })
+    return
+  }
+
+  next()
 }
 
-//router.use(checker)
-
-
-router.get("/", async (request,response) => {
-    let shows = await Show.findAll({});
-    response.json(shows);
+router.get('/', async (request, response) => {
+  let shows = await Show.findAll({})
+  response.json(shows)
 })
 
-router.get("/:showid", async (request,response) => {
-    const showid = request.params.showid;
-    const show = await Show.findByPk(showid);
-    response.json(show)
+router.get('/:showid', async (request, response) => {
+  const showid = request.params.showid
+  const show = await Show.findByPk(showid)
+  response.json(show)
 })
 
-router.get("/genres/:genre", async (request,response) => {
-    const genre = request.params.genre;
-    const show = await Show.findAll({where: 
-        {genre:genre
-        }
-    });
-    response.json(show)
+router.get('/genres/:genre', async (request, response) => {
+  const genre = request.params.genre
+  const show = await Show.findAll({ where: { genre: genre } })
+  response.json(show)
 })
 
-router.put("/:showid/rating/:rating", async (request,response) => {
-    await Show.update({rating:request.params.rating},{
-        where:{
-            id:request.params.showid
-        }
-    })
-    const show = await Show.findByPk(request.params.showid);
-    response.json(show)
+router.put('/:showid/rating/:rating', async (request, response) => {
+  await Show.update(
+    { rating: request.params.rating },
+    {
+      where: {
+        id: request.params.showid
+      }
+    }
+  )
+  const show = await Show.findByPk(request.params.showid)
+  response.json(show)
 })
 
 // Trying out custom middleware (at top of file) to validate the parameters
-router.put("/:showid/status/:status", checker, async (request,response) => {
-    await Show.update({status:request.params.status},{
-        where:{
-            id:request.params.showid
-        }
-    })
-    const show = await Show.findByPk(request.params.showid);
-    response.json(show)
+router.put('/:showid/status/:status', checker, async (request, response) => {
+  await Show.update(
+    { status: request.params.status },
+    {
+      where: {
+        id: request.params.showid
+      }
+    }
+  )
+  const show = await Show.findByPk(request.params.showid)
+  console.log(show)
+  response.json(show)
 })
 
-router.delete("/:showid", async (request,response) => {
-    const deleted = await Show.findByPk(request.params.showid)
-    await Show.destroy({
-        where:{
-            id:request.params.showid
-        }
-    })
-    response.json(deleted);
+router.delete('/:showid', async (request, response) => {
+  const deleted = await Show.findByPk(request.params.showid)
+  await Show.destroy({
+    where: {
+      id: request.params.showid
+    }
+  })
+  response.json(deleted)
 })
 
-module.exports = router;
+module.exports = router
